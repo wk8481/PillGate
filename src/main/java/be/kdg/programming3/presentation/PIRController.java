fb4b604bfc9ef44bf9ae87c68fd29b2f5302c0af
Emran Hamdard <emran.hamdard@student.kdg.be>
2023-11-11 11:54:40 +0000

    added files

--- src/main/java/be/kdg/programming3/presentation/PIRController.java
+++ src/main/java/be/kdg/programming3/presentation/PIRController.java
@@ -1,15 +1,15 @@
 package be.kdg.programming3.presentation;
 
-import be.kdg.programming3.service.SensorService;
+import be.kdg.programming3.service.PIRSensorServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class PIRController {
-    private final SensorService sensorService;
+    private final PIRSensorServiceImpl sensorService;
 
-    public PIRController(SensorService sensorService) {
+    public PIRController(PIRSensorServiceImpl sensorService) {
         this.sensorService = sensorService;
     }
 
