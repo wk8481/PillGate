630d9e57129032e64ab5492113b8fa60db36e4cc
Manami Hayashi <manami.hayashi@student.kdg.be>
2023-12-01 20:50:53 +0100

    create new blanch that was fixed database and website issues

---
+++ build/resources/main/js/dropdown-menu.js
@@ -0,0 +1,78 @@
+// for dropdown menu
+
+// let dropbtn = document.querySelector('.dropbtn');
+//
+// dropbtn.addEventListener('click', toggleDropdown);
+//
+// function toggleDropdown() {
+//     let dropdown = document.getElementById('myDropdown');
+//     dropdown.classList.toggle("show");
+// }
+//
+// window.addEventListener('click', function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//         let dropdowns = document.getElementsByClassName('dropdown-content');
+//         for (let i = 0; i < dropdowns.length; i++) {
+//             let openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// });
+
+// scroll to section when clicking on a link
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+// hide logo on scroll
+document.addEventListener('DOMContentLoaded', function() {
+    let logoContainer = document.querySelector('.logo-container');
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 0) {
+            logoContainer.classList.add('hide-logo');
+        } else {
+            logoContainer.classList.remove('hide-logo');
+        }
+    });
+});
+
+// show section2 after scrolling
+window.addEventListener('scroll', function() {
+    let logoHeight = document.getElementById('navbar').offsetHeight;
+    let section2 = document.getElementById('section2');
+    let scrollPosition = window.scrollY;
+
+    let triggerPoint = logoHeight * 1.5;
+
+    if (scrollPosition > triggerPoint) {
+        section2.classList.add('visible-section');
+        section2.classList.remove('hidden-section');
+    } else {
+        section2.classList.add('hidden-section');
+        section2.classList.remove('visible-section');
+    }
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('scroll', function() {
+        let scrollPosition = window.scrollY;
+
+        // Adjust the value below to control when the main logo disappears
+        let triggerPoint = document.querySelector('.logo-container').offsetHeight;
+
+        // Toggle visibility of logos based on scroll position
+        document.querySelector('.logo-container').style.display = scrollPosition > triggerPoint ? 'none' : 'block';
+        document.querySelector('.smaller-logo').style.display = scrollPosition > triggerPoint ? 'block' : 'none';
+    });
+});
---
+++ build/resources/main/js/reminder.js
@@ -0,0 +1,118 @@
+
+
+function checkReminder(){
+    $.get("/api/checkreminder", function(data){
+        if( data ==="true"){
+            alert("It's time to take your medication!")
+            location.reload();
+        }
+    });
+}
+// check for a reminder every minute
+setInterval(checkReminder, 60000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // subscribe to a websocket topic
+// let socket = new WebSocket('/websocket');
+// socket.onmessage = function (event){
+//     let reminderMessage = event.data;
+//     showAlert(remindeMessage);
+// }
+//
+//
+//
+// function showAlert(message) {
+//     // this shows pop up windows for reminder
+//     alert(message);
+// }
+//
+// function pollReminders() {
+//     // Poll for new reminders every 60 seconds
+//     setInterval(function () {
+//         fetch('/pollReminders')
+//             .then(response => {
+//                 if (!response.ok) {
+//                     throw new Error('Network response was not ok');
+//                 }
+//                 return response.json();
+//             })
+//             .then(data => {
+//                 if (data && data.message) {
+//                     showAlert('Received reminder: ' + data.message);
+//                 } else {
+//                     console.warn('Received unexpected data:', data);
+//                 }
+//             })
+//             .catch(error => {
+//                 console.error('Error polling reminders:', error.message);
+//             });
+//     }, 60000);
+// }
+//
+// function pollReminders() {
+//     // Poll for new reminders every 60 seconds
+//     setInterval(function () {
+//         fetch('/pollReminders')
+//             .then(response => response.json())
+//             .then(data => {
+//                 if (data && data.message) {
+//                     showAlert('Received reminder: ' + data.message);
+//                 }
+//             })
+//             .catch(error => console.error('Error polling reminders: ', error));
+//     }, 60000); // Corrected the interval to 60 seconds
+// }
+
+// // start polling when the page is loaded
+// document.addEventListener('DOMContentLoaded', function () {
+//     // call the pollReminders function when the DOM content is fully loaded
+//     pollReminders();
+// });
+
+
+
+
+
+// function showAlert(message){
+// // this shows pop up windows for reminder
+//     alert(message);
+// }
+//
+// function pollReminders(){
+//
+//     // Poll for new reminders every 60 seconds
+//     setInterval(function(){
+//         fetch('/pollReminders')
+//             .then(responce => responce.json())
+//             .then(data => {
+//                 if (data && data.message ){
+//                     showAlert('Received reminder: '+ data.message);
+//                 }
+//             })
+//             .catch (error => console.error('Error polling reminders: ', error));
+//         }, 6000);
+// }
+//
+// // start polling when the page is loaded
+// document.addEventListener('DOMContentLoaded', function(){
+//     // call the pollReminders function when the DOM content is fully loaded
+//     pollReminders();
+// });
+
