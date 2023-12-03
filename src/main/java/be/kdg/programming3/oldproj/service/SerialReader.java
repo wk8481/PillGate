1c9f814cb78ab58821dbfc67aa642a6d68a907ed
Emran Hamdard <105441578+emran-ai@users.noreply.github.com>
2023-12-03 22:56:55 +0100

    added loadssensor

--- src/main/java/be/kdg/programming3/oldproj/service/SerialReader.java
+++
@@ -1,8 +0,0 @@
-package be.kdg.programming3.oldproj.service;
-
-import java.io.IOException;
-
-public interface SerialReader {
-    void readArduinoData(String portName);
-    void disconnect()throws IOException;
-}
